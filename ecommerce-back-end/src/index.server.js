55902b5b5c188fb7ed47399f327e110126a78eb0
Naveen Verma <nveenverma@gmail.com>
2021-05-20 10:13:01 +0400

    Initial Commit - Backend Signup and Signin routes created

---
+++ ecommerce-back-end/src/index.server.js
@@ -0,0 +1,36 @@
+// Node Modules
+const express = require('express')
+const env = require('dotenv')
+const app = express();
+const mongoose = require('mongoose')
+
+// routes
+const authRoutes = require("./routes/auth")
+
+// Environment variable or you can say constants
+env.config();
+
+// MongoDB Connection
+const MONGODB_URI = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PWD}@cluster0.yiuxu.mongodb.net/${process.env.MONGODB_DATABASE}?retryWrites=true&w=majority`;
+
+mongoose.connect(
+    MONGODB_URI, 
+    {
+        useNewUrlParser : true,
+        useUnifiedTopology : true,
+        useCreateIndex : true
+    }
+).then(() => {
+    console.log("Database Connected");
+})
+
+// To parse the data passed in the requests, we use following middleware
+app.use(express.json())
+    
+// endpoint '/api' using express router
+app.use('/api', authRoutes);
+
+// Listening to the server on below port
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on ${process.env.PORT}`)
+});
---
+++ ecommerce-back-end/src/routes/auth.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const { signup, signin } = require('../controller/auth');
+const router = express.Router();
+
+router.post('/signin', signin);
+router.post('/signup', signup);
+
+module.exports = router;
